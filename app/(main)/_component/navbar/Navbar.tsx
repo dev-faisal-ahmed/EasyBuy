7990413d090f207a5c27b26ce0e8e42945df6f1e
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-03-22 01:59:30 +0600

    navbar responsive

--- app/(main)/_component/navbar/Navbar.tsx
+++ app/(main)/_component/navbar/Navbar.tsx
@@ -8,6 +8,13 @@ import { NavLink } from './NavLink';
 import { navLinks } from './navLinks.data';
 import { ShoppingCart as ShoppingCartIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { CgMenuLeftAlt } from 'react-icons/cg';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 export function Navbar() {
   const pathName = usePathname();
@@ -18,15 +25,39 @@ export function Navbar() {
 
         {/* links */}
         <div className='flex items-center justify-end gap-6'>
-          {navLinks.map((link) => (
-            <NavLink key={link.href} {...link} currentPath={pathName} />
-          ))}
-
+          <div className='hidden items-center gap-6 lg:flex'>
+            {navLinks.map((link) => (
+              <NavLink key={link.href} {...link} currentPath={pathName} />
+            ))}
+          </div>
           <Link href={'/cart'}>
-            <ShoppingCartIcon />
+            <ShoppingCartIcon size={22} />
           </Link>
 
-          <Link href={'/login'}>
+          <div className='lg:hidden'>
+            <Sheet>
+              <SheetTrigger asChild>
+                <span className='cursor-pointer text-2xl'>
+                  <CgMenuLeftAlt />
+                </span>
+              </SheetTrigger>
+              <SheetContent>
+                <div className='mt-5 flex flex-col gap-3'>
+                  {navLinks.map((link) => (
+                    <SheetClose key={link.href}>
+                      <NavLink {...link} currentPath={pathName} />
+                    </SheetClose>
+                  ))}
+
+                  <Link href={'/login'}>
+                    <Button className='mt-3 w-full'>Login</Button>
+                  </Link>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+
+          <Link className='hidden lg:block' href={'/login'}>
             <Button>Login</Button>
           </Link>
         </div>
