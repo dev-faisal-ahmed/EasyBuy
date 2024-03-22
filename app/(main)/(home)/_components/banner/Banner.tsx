dd62ca5e327c6c4d1343e77f726aabe57c530e39
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-03-23 02:45:19 +0600

    responsive issues fix with banner

--- app/(main)/(home)/_components/banner/Banner.tsx
+++ app/(main)/(home)/_components/banner/Banner.tsx
@@ -6,12 +6,14 @@ export function Banner() {
   return (
     <Container className='mt-8 w-full'>
       <div
-        className='flex min-h-[500px] flex-col justify-center rounded-md bg-cover bg-no-repeat px-32 text-white'
+        className='flex min-h-[500px] flex-col justify-center rounded-md bg-cover bg-no-repeat px-8 text-center text-white lg:px-32 lg:text-left'
         style={{ backgroundImage: `url(/images/banner.webp)` }}
       >
-        <h3 className='text-4xl font-semibold'>Short Edition</h3>
-        <h2 className='mb-3 mt-1 text-5xl font-medium'>Red Special Edition</h2>
-        <p className='mb-12 text-lg'>
+        <h3 className='text-2xl font-semibold lg:text-4xl'>Short Edition</h3>
+        <h2 className='mb-3 mt-1 text-3xl font-medium lg:text-5xl'>
+          Red Special Edition
+        </h2>
+        <p className='mb-12 text-base lg:text-lg'>
           Wireless Connection With TV, Computer, Laptop
         </p>
         <Link href={'/products'}>
