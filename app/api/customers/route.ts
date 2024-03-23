78e65f80bf66138d7ff0361cd0e3213df08fd597
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-03-23 21:13:21 +0600

    api router deleted

--- app/api/customers/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { customers } from '@/lib/data/customers.data';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  return NextResponse.json(customers);
-}
--- app/api/products/route.ts
+++
@@ -1,8 +0,0 @@
-import { products } from '@/lib/data/products.data';
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  return NextResponse.json(products);
-}
